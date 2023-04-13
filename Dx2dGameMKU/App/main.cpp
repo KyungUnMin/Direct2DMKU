2a545766bdb8eca6b2b89f6e11719e59a47dacc1
KyungUnMin <insetour@naver.com>
2023-04-13 20:02:05 +0900

    민경운 : DirectShow 알아보기

--- Dx2dGameMKU/App/main.cpp
+++ Dx2dGameMKU/App/main.cpp
@@ -7,14 +7,14 @@
 #pragma comment(lib, "GameEngineCore.lib")
 #pragma comment(lib, "GameEngineContents.lib")
 
-//프라그마 코멘츠를 안 했는데 어떻게 다른 외부 포함 lib를 사용할 수 있을까?
-//아모른직다
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
+	
+
 	float4x4 Parent;
 
 	Parent.ArrVector[3] = { -100, -100 };
